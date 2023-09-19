7ba0873b657c2e35aa584fede545512afaefb8fa
Роман Беспалов <rbespalov.97@inbox.ru>
2023-09-20 01:08:46 +0400

    fix_key_3

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY_ENV')  # 2
+SECRET_KEY = os.getenv('SECRET_KEY_ENV')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'foodgramblog.myddns.me', '130.193.40.153']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS_ENV').split()
 
 INSTALLED_APPS = [
     'recipes.apps.RecipesConfig',
