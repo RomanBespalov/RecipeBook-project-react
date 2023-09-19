968d9448fae2040a64afb5c3ec663b743151751b
Роман Беспалов <rbespalov.97@inbox.ru>
2023-09-19 23:42:35 +0400

    new settings

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -4,8 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = ('django-insecure-2w=5otnn@-&0n='
-              '(sct&#4@3wk=&c7$!5_aip_xr55uug3f-^-h')
+SECRET_KEY = ('django-insecure-2w=5otnn@-&0n=(sct&#4@3wk=&c7$!5_aip_xr55uug3f-^-h')
 
 DEBUG = True
 
@@ -70,20 +69,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'UserAttributeSimilarityValidator'),
+        'NAME': ('django.contrib.auth.password_validation.UserAttributeSimilarityValidator'),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'MinimumLengthValidator'),
+        'NAME': ('django.contrib.auth.password_validation.MinimumLengthValidator'),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'CommonPasswordValidator'),
+        'NAME': ('django.contrib.auth.password_validation.CommonPasswordValidator'),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
-                 'NumericPasswordValidator'),
+        'NAME': ('django.contrib.auth.password_validation.NumericPasswordValidator'),
     },
 ]
 
